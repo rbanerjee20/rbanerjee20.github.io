ddc2f2905dd3683ee54ea5bfc7b5acf2df48e879
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-08-30 15:53:22 +0100

    Fix path

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const converter = new showdown.Converter();
 
 function populateFromData(id, path) {
-    $.get("data/" + path, (data) => {
+    $.get("/data/" + path, (data) => {
         console.log(data)
         $(`#${id}`).html(converter.makeHtml(data))
     })
