abc447007d33947b32f4a93bb040f71ed8ed970d
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-08-30 16:54:20 +0100

    Add project button

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const converter = new showdown.Converter();
 function populateFromData(id, path) {
     $.get("/data/" + path, (data) => {
         $(`#${id}`).html(converter.makeHtml(data))
+        // Plug in new styles
+        $('.md-button').addClass('btn btn-primary');
+        $('.md-button').parent().addClass('p-right')
     })
 }
 
